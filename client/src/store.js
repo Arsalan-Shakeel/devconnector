ee38740510627cd7db8cb035a4e1f575ea05748a
Arsalan Shakeel <arsalan_shakeel@yahoo.com>
2018-12-18 02:23:01 +0500

    modified for IE - store.js

--- client/src/store.js
+++ client/src/store.js
@@ -3,18 +3,17 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const initialState = {};
-
 const middleware = [thunk];
-
-const store = createStore(
-	rootReducer,
-	initialState,
-	compose(
-		applyMiddleware(...middleware),
-		process.env.NODE_ENV !== 'production' &&
-			window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
-			window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancer = composeEnhancers(
+	applyMiddleware(...middleware)
+	// other store enhancers if any
 );
 
+const store = createStore(rootReducer, initialState, enhancer);
+
 export default store;
+
+//Code was taken from this link -
+//https://github.com/zalmoxisus/redux-devtools-extension#12-advanced-store-setup
+//https://github.com/zalmoxisus/redux-devtools-extension/issues/320
