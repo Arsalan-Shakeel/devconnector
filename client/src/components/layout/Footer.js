be876fb1e4c444d47203458a1b294e9428e676d1
Arsalan Shakeel <arsalan_shakeel@yahoo.com>
2019-01-20 15:42:09 +0500

    Commit on Jan 20, 2019

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 export default () => {
-  return (
-    <footer className="bg-dark text-white mt-5 p-4 text-center">
-      Copyright &copy; {new Date().getFullYear()} DevConnector
-    </footer>
-  );
+	return (
+		<footer id="main-footer" className="text-center p-4">
+			<hr className="hr-styling" />
+			<div clasName="container">
+				<div className="row">
+					<div className="col">
+						<p>
+							<strong>Copyright &copy; {new Date().getFullYear()} D4Styles</strong>
+						</p>
+					</div>
+				</div>
+			</div>
+		</footer>
+	);
 };
