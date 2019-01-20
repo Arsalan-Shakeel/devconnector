7256c6d2009ac0b0eb1e438be263de048714ff00
Arsalan Shakeel <arsalan_shakeel@yahoo.com>
2019-01-20 13:01:50 +0500

    Commit on Jan 20, 2019

--- client/src/components/layout/Navbar_bkp.js
+++ client/src/components/layout/Navbar_bkp.js
@@ -16,28 +16,25 @@ class Navbar extends Component {
 	render() {
 		const { isAuthenticated, user } = this.props.auth;
 
-		const homeLink = (
-			<li className="nav-item active">
-				<Link className="nav-link" to="/profiles">
-					<i className="fas fa-home fa-1x" style={{ color: 'rgba(238, 20, 111, 0.685)' }} />{' '}
-					<strong>Home</strong>
-				</Link>
-			</li>
-		);
-
 		const authLinks = (
 			<ul className="navbar-nav ml-auto">
-				{homeLink}
 				<li className="nav-item">
-					<Link className="nav-link" to="/dashboard">
-						<i className="fas fa-binoculars fa-1x" style={{ color: 'rgba(238, 20, 111, 0.685)' }} />{' '}
-						<strong>Explore</strong>
+					<Link className="nav-link" to="/profiles">
+						<i className="fas fa-home fa-1x" style={{ color: 'rgba(238, 20, 111, 0.685)' }} />{' '}
+						<strong>HOME</strong>
 					</Link>
 				</li>
 				<li className="nav-item">
 					<Link className="nav-link" to="/feed">
-						<i className="fas fa-cart-plus fa-1x" style={{ color: 'rgba(238, 20, 111, 0.685)' }} />{' '}
-						<strong>Buy</strong>
+						<i className="fab fa-delicious fa-1x" style={{ color: 'rgba(238, 20, 111, 0.685)' }} />{' '}
+						<strong>Post Feed</strong>
+					</Link>
+				</li>
+
+				<li className="nav-item">
+					<Link className="nav-link" to="/dashboard">
+						<i className="fab fa-dribbble fa-1x" style={{ color: 'rgba(238, 20, 111, 0.685)' }} />{' '}
+						<strong>Dashboard</strong>
 					</Link>
 				</li>
 				<li className="nav-item">
@@ -51,17 +48,14 @@ class Navbar extends Component {
 
 		const guestLinks = (
 			<ul className="navbar-nav ml-auto">
-				{homeLink}
 				<li className="nav-item">
 					<Link className="nav-link" to="/register">
-						<i className="fas fa-user-plus fa-1x" style={{ color: 'rgba(238, 20, 111, 0.685)' }} />{' '}
-						<strong>Register</strong>
+						Sign Up
 					</Link>
 				</li>
 				<li className="nav-item">
 					<Link className="nav-link" to="/login">
-						<i className="fas fa-sign-in-alt fa-1x" style={{ color: 'rgba(238, 20, 111, 0.685)' }} />{' '}
-						<strong>Login</strong>
+						Login
 					</Link>
 				</li>
 			</ul>
@@ -81,31 +75,13 @@ class Navbar extends Component {
 						/>{' '}
 						<strong> Your Design Destination!</strong>
 					</Link>
-					<button
-						className="navbar-toggler"
-						type="button"
-						data-toggle="collapse"
-						data-target="#mobile-nav"
-						aria-controls="mobile-nav"
-						aria-expanded="false"
-						aria-label="Toggle navigation"
-					>
+					<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
 						<span className="navbar-toggler-icon" />
 					</button>
+
 					<div className="collapse navbar-collapse" id="mobile-nav">
 						{isAuthenticated ? authLinks : guestLinks}
 					</div>
-					<form className="form-inline my-2 my-lg-0">
-						<input
-							className="form-control mr-sm-2"
-							type="search"
-							placeholder="Search"
-							aria-label="Search"
-						/>
-						<button className="btn btn-default my-2 my-sm-0" type="submit">
-							<i className="fas fa-search fa-1x" style={{ color: 'rgba(238, 20, 111, 0.685)' }} />
-						</button>
-					</form>
 				</div>
 			</nav>
 		);
